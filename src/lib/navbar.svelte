e7a7b050d3284dd7c25bf068175bd481f8dbe335
Lukas Schmitt <lukas.a.schmitt@gmail.com>
2024-09-16 21:03:35 +0200

    add outline to dropdown

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -11,7 +11,9 @@
       <div tabindex="0" role="button" class="btn btn-ghost rounded-btn">
         <span class="iconify mdi--menu w-6 h-6"></span>
       </div>
-      <ul class="menu dropdown-content bg-base-300 rounded-box z-[1] w-64 mt-2 p-2 shadow">
+      <ul
+        class="menu dropdown-content bg-base-300 rounded-box z-[1] w-64 mt-2 ml-2 shadow p-2 outline outline-neutral-600"
+      >
         <ProjectMenu {projectConfigurations}></ProjectMenu>
       </ul>
     </div>
